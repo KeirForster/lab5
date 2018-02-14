80db294094934a1074db99086d87066222163f8a
Keir Forster <keirforster@gmail.com>
2018-02-13 17:23:30 -0800

    completed job 3

---
+++ application/controllers/Views.php
@@ -0,0 +1,46 @@
+<?php
+
+class Views extends Application
+{
+
+    public function index()
+    {
+        $this->data['pagetitle'] = 'Ordered TODO List';
+        $tasks = $this->tasks->all();   // get all the tasks
+        $this->data['content'] = 'Ok'; // so we don't need pagebody
+        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+        $this->data['rightside'] = $this->tasks->makeCategorizedPanel($tasks);
+
+        $this->render('template_secondary');
+    }
+
+    function makePrioritizedPanel($tasks) {
+
+        // extract the undone tasks
+        foreach ($tasks as $task)
+        {
+            if ($task->status != 2)
+                $incomplete[] = $task;
+        }
+        usort($incomplete, "orderByPriority");
+
+        // substitute the priority name
+        foreach ($incomplete as $task)
+            $task->priority = $this->app->priority($task->priority);
+
+//        $converted[] = (array) $task;
+        $parms = ['display_tasks' => $incomplete];
+        return $this->parser->parse('by_priority',$parms,true);
+    }
+}
+
+// return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
+function orderByPriority($a, $b)
+{
+    if ($a->priority > $b->priority)
+        return -1;
+    elseif ($a->priority < $b->priority)
+        return 1;
+    else
+        return 0;
+}
