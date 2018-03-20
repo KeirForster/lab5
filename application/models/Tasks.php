25eaac993efb895b79b24916c7095b3885d3175e
Keir Forster <keirforster@gmail.com>
2018-03-20 16:08:47 -0700

    override load method

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,10 +1,10 @@
 <?php
 
-class Tasks extends CSV_Model
+class Tasks extends XML_Model
 {
     public function __construct()
     {
-        parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+        parent::__construct(APPPATH . '../data/tasks.xml', 'id');
     }
 	
 		// provide form validation rules
@@ -58,4 +58,87 @@ function orderByCategory($a, $b)
         return 1;
     else
         return 0;
+}
+
+function load()
+{
+		if (($tasks = simplexml_load_file($this->_origin)) !== FALSE)
+		{
+			foreach ($tasks as $task) {
+				$record = new stdClass();
+				$record->id = (int) $task->id;
+				$record->task = (string) $task->task;
+				$record->priority = (int) $task->priority;
+				$record->size = (int) $task->size;
+				$record->group = (int) $task->group;
+				$record->deadline = (string) $task->deadline;
+				$record->status = (int) $task->status;
+				$record->flag = (int) $task->flag;
+
+				$this->_data[$record->id] = $record;
+			}
+		}
+
+		// rebuild the keys table
+		$this->reindex();
+
+    if (file_exists(realpath($this->_origin))) {
+
+        $this->xml = simplexml_load_file(realpath($this->_origin));
+        if ($this->xml === false) {
+            // error so redirect or handle error
+            header('location: /404.php');
+            exit;
+        }
+
+        $xmlarray =$this->xml;
+
+        //if it is empty;
+        if(empty($xmlarray)) {
+            return;
+        }
+
+        //get all xmlonjects into $xmlcontent
+        $rootkey = key($xmlarray);
+        $xmlcontent = (object)$xmlarray->$rootkey;
+
+        $keyfieldh = array();
+        $first = true;
+
+        //if it is empty;
+        if(empty($xmlcontent)) {
+            return;
+        }
+
+        $dataindex = 1;
+        $first = true;
+        foreach ($xmlcontent as $oj) {
+            if($first){
+                foreach ($oj as $key => $value) {
+                    $keyfieldh[] = $key;
+                    //var_dump((string)$value);
+                }
+                $this->_fields = $keyfieldh;
+            }
+            $first = false;
+
+            //var_dump($oj->children());
+            $one = new stdClass();
+
+            //get objects one by one
+            foreach ($oj as $key => $value) {
+                $one->$key = (string)$value;
+            }
+            $this->_data[$dataindex++] =$one;
+        }
+
+
+        //var_dump($this->_data);
+    } else {
+        exit('Failed to open the xml file.');
+    }
+
+    // --------------------
+    // rebuild the keys table
+    $this->reindex();
 }
